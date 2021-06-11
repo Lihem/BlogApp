149265c4e2d319cda7d4d280a8a784cded766fdb
Lihem <mlhcsgn10@gmail.com>
2021-06-11 14:34:53 +0300

    headerRight icon gorunmuyor

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React,{useContext} from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import {Context} from '../context/BlogContext'
+
+const CreateScreen= () => {
+
+    return <View>
+        <Text>create Screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default CreateScreen
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -2,25 +2,38 @@ import React, {useContext} from 'react'
 import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-native'
 import {Context} from '../context/BlogContext' // 2 farkli yerden Context gelirse {Context as BlogContext} seklinde yeniden isimlendirebilirdik
 import { Feather } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons'; 
 
-const IndexScreen = () =>{
+const IndexScreen = ({navigation}) =>{
     const {state,addBlogPost, deleteBlogPost} = useContext(Context) //data ve addBlogPost isimleri ayni olmali gonderilen ile 
     
-    return <View style = {{flex: 1}}>
-        <Button title='Add Post' onPress={addBlogPost}/>
-        <FlatList
-            data={state}
-            keyExtractor = { (blogPost) => blogPost.title}
-            renderItem= {({item}) => {
-                return <View style={styles.row}>
-                    <Text style={styles.title}>{item.title} - {item.id}</Text>
-                    <TouchableOpacity onPress= {() => {deleteBlogPost(item.id)}}>
-                        <Feather name="trash-2" style={styles.icon}/>  
-                    </TouchableOpacity>
-                </View>
-            }}
-        />
-    </View>
+    return <View>
+            <Button title='Add Post' onPress={addBlogPost}/>
+            <FlatList
+                data={state}
+                keyExtractor = { (blogPost) => blogPost.title}
+                renderItem= {({item}) => {
+                    return <View style={styles.row}>
+                        <TouchableOpacity onPress = {() => navigation.navigate('Show',{id: item.id})}>
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress= {() => {deleteBlogPost(item.id)}}>
+                            <Feather name="trash-2" style={styles.icon}/>  
+                        </TouchableOpacity>
+                    </View>
+                }}
+            />
+        </View>
+
+}
+
+IndexScreen.navigationOptions = ({navigation}) => {
+    return {
+        headerRight: <TouchableOpacity onPress = {() => navigation.navigate('Create')}>
+                <AntDesign name="pluscircleo" size={24} color="black" />
+            </TouchableOpacity>
+        
+    }
 }
 
 const styles = StyleSheet.create({
